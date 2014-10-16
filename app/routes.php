d8933db438c7d6166bb535ca9d869659ba4cb68f
CP <cpalmit@users.noreply.github.com>
2014-10-15 23:59:16 -0400

    basic routes; install bad cow and faker packages

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,38 @@
 |
 */
 
+
+//homepage
 Route::get('/', function()
 {
-	return View::make('hello');
+	return "This will be the homepage";
+	//return View::make('index');
+});
+
+##############################
+//if I decide not to have both forms on the homepage...
+
+//display lorem ipsum generator
+Route::get('/lipsum', function (){
+	
+});
+
+
+//display users generator
+Route::get('/users', function (){
+	
+});
+###############################
+
+//lorem ipsum form submission
+Route::post('/lipsum', function() {
+
+	
+});
+
+
+//user form submission
+Route::post('/users', function() {
+
+
 });
